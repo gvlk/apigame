9cb490d3a90f07662127346af8a6bea78f7a6168
Gui <88752991+gvlk@users.noreply.github.com>
2024-12-07 18:10:13 -0300

    new instructions

--- apigame/routes.py
+++ apigame/routes.py
@@ -5,3 +5,15 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def hello_world():
     return render_template('index.html', message="Hello, Flask!")
+
+@main.route('/play')
+def play():
+    return ""
+
+@main.route('/ranking')
+def ranking():
+    return ""
+
+@main.route('/avatar')
+def avatar():
+    return ""
