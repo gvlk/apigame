3f4297d6fe9abd4bd7bb712dc65962854cde7886
Gui <88752991+gvlk@users.noreply.github.com>
2024-12-15 19:51:48 -0300

    live question in the database count

--- apigame/routes/debug.py
+++ apigame/routes/debug.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 from apigame.services.question_service import question_db
 from apigame.services.session_service import session_db
@@ -20,3 +20,12 @@ def debug_session():
 @debug.route('/question')
 def debug_question():
     return question_db.questions.to_dict()
+
+
+@debug.route('/question-count', methods=['GET'])
+def get_question_count():
+    """
+    API endpoint to get the total number of questions in the database.
+    """
+    total_questions = question_db.get_question_count()
+    return jsonify({'total_questions': total_questions})
