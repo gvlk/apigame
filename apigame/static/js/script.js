b8bb2c351ef44c5fa8fd9aa2f859bdc06322ca1d
Gui <88752991+gvlk@users.noreply.github.com>
2024-12-14 22:18:55 -0300

    scripts file renamed

--- apigame/static/js/script.js
+++
@@ -1,7 +0,0 @@
-function showGameOptions() {
-    // Hide the 'Jogar' button and show the new options
-    document.getElementById('play-button').style.display = 'none';
-    document.getElementById('ranking-button').style.display = 'none';
-    document.getElementById('game-options').style.display = 'block';
-}
-
